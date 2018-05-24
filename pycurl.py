18f4fa9bcb7918e4f5b0dd1721485025ba93ea21
welkeyever <wenju.gao124@gmail.com>
2018-05-24 21:24:14 +0800

    add a function to pycurl: normal 'curl' in Linux.

--- pycurl.py
+++ pycurl.py
@@ -9,7 +9,7 @@
 
 @time: 2018/5/18 上午12:23
 
-@desc:无返回值的curl一个网址，第一个参数为网址，第二个参数为curl的次数，第三个参数为可选参数，设定每一个curl的间隔秒数
+@desc:无返回值的curl一个网址，第一个参数为网址，第二个参数为curl的次数，第三个参数为可选参数，设定每一个curl的间隔秒数,若只有地址则退化为普通curl。
 
 '''
 
@@ -17,10 +17,16 @@ import sys, urllib2, time
 if len(sys.argv) == 1 or len(sys.argv) > 4:
     print('Usage: ./pycurl.py http://abc.com 10 1')
     sys.exit(0)
-if len(sys.argv) == 2:
-    print('need a number to repeat')
-    sys.exit(0)
 url = sys.argv[1]
+
+
+if len(sys.argv) == 2:
+    req = urllib2.Request(url)
+    res = urllib2.urlopen(req)
+    text = res.read().decode("utf-8")
+    print(text)
+    sys.exit(0)
+
 times = sys.argv[2]
 count = 0
 
